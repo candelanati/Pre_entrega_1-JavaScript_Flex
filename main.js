8a9ad68c0391e37b9b8556af07478760d9b9a525
candelanati <candelanti23@gmail.com>
2024-07-24 16:53:41 -0300

    changes for paying section (numbers to text in metodo / confirm for exiting without payment or staying to choose another method)

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let cuadrosDisponibles = ["happier than ever", "across the spiderverse", "currents", "to pimp a butterfly"]
 console.log(cuadrosDisponibles)
 
+let metodosPago =["transferencia","mercado pago","efectivo","débito","crédito"]
+
 let precios = [20000, 15000, 18000, 23000]
 let cuadro
 let total = 0
@@ -18,21 +20,26 @@ console.log(nuevoCuadro)
 function pago (metodo){
     switch(metodo){
         case "credito":
+        case "5":    
             total = total + (total * 0.15)
             console.log(total)
             break
         case "transferencia":
+        case "1":
             total = total - (total * 0.15)
             console.log(total)
             break
         case "efectivo":
+        case "3":
             total = total - (total * 0.15)
             console.log(total)
             break
         case "mercado pago":
+        case "2":
             // no cambia el precio
             break
         case "debito":
+        case "4":
             // no cambia el precio
             break
         default:
@@ -98,9 +105,29 @@ if(!exit){
             console.log(metodo)
             console.log(pago(metodo))
             pagoRealizado=true
+            //si es numero pasa la variable metodo a su equivalente, ej: 1 = "transferencia"
+            if(!isNaN(metodo)&&Number(metodo)>=1&&Number(metodo)<=5){
+                let numeroRespuestaPago = Number(metodo)
+                console.log(numeroRespuestaPago)
+                metodo = metodosPago[numeroRespuestaPago-1]
+            }
             if(confirm("usted se encuentra por abonar " + respuestaPago[1] + " por el siguiente medio de pago: " + metodo)){
                 alert("compra realizada con exito!")
-                
+            }else{
+                if(confirm("desea retirarse sin completar su pedido?")){
+                    //el pago no esta realizado pero se activa para salir del while
+                    pagoRealizado=true
+                }else{
+                    alert("debe ingresar un metodo de pago")
+                    pagoRealizado=false
+                }
+            }
+        }else{
+            if(confirm("desea retirarse sin completar su pedido?")){
+                pagoRealizado=true
+            }else{
+                //el pago no esta realizado pero se activa para salir del while
+                alert("debe ingresar un metodo de pago")
             }
         }
     }
